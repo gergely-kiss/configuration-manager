ac3b8e5117e799e7fb5a5c5c8173c1888b0ad186
Kiss Gergely <kiss-gergely@outlook.com>
2020-04-25 11:31:53 +0100

    init commit

---
+++ src/main/java/uk/gergely/kiss/configurationprovider/ConfigurationProviderApplication.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.configurationprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gergely/kiss/configurationprovider/ConfigurationProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gergely.kiss.configurationprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
