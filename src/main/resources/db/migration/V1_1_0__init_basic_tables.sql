63891cf689fdee6bc43bf9e592a9389f40d1db8b
Kiss Gergely <kiss-gergely@outlook.com>
2020-04-27 10:07:53 +0100

    flyway config. pom update. properties update.

--- src/main/resources/db/migration/V1_1_0__init_basic_tables.sql
+++ src/main/resources/db/migration/V1_1_0__init_basic_tables.sql
@@ -1,17 +1,17 @@
-create table "config-manager".registered_application
+create table registered_application
 (
     application_id varchar(255) not null,
     password varchar(255)
 );
 
 create unique index registered_application_application_id_uindex
-    on "config-manager".registered_application (application_id);
+    on registered_application (application_id);
 
-alter table "config-manager".registered_application
+alter table registered_application
     add constraint registered_application_pk
         primary key (application_id);
 
-create table "config-manager".configuration
+create table configuration
 (
     id serial not null,
     application_id varchar(255) not null,
@@ -20,8 +20,8 @@ create table "config-manager".configuration
 );
 
 create unique index configuration_id_uindex
-    on "config-manager".configuration (id);
+    on configuration (id);
 
-alter table "config-manager".configuration
+alter table configuration
     add constraint configuration_pk
         primary key (id);
